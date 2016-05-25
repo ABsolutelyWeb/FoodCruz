b41e205528c64c5ad976fb128d5c48241535f0aa
Abhishek <awittlegupta@gmail.com>
2016-05-25 01:46:18 +0000

    Bootstrap Sidebar Added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,43 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+
+//////////////////////////////////////////////////////////////////
+//  SIDEBAR START
+//////////////////////////////////////////////////////////////////
+$(document).ready(function () {
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = false;
+
+    trigger.click(function () {
+      hamburger_cross();      
+    });
+
+    function hamburger_cross() {
+
+      if (isClosed == true) {          
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {   
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = true;
+      }
+  }
+  
+  $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });  
+});
+
+//////////////////////////////////////////////////////////////////
+//  SIDEBAR END
+//////////////////////////////////////////////////////////////////
