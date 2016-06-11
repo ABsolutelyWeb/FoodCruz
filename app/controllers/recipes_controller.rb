27d5d8a281cd2766f09aa561672e0c85f0618310
Abhishek <awittlegupta@gmail.com>
2016-06-11 15:46:41 +0000

    New sidebar added. Background img link replaced with img path

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,30 @@
+class RecipesController < ApplicationController
+  def index
+    # Make all recipe objects (from DB) available in the index action.
+    @recipes = Recipe.all
+  end
+  
+  def create
+    
+  end
+  
+  def new
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def show
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,52 @@
 Rails.application.routes.draw do
   # Root route set to home action
   root 'pages#home'
+  
   # If there's a GET request for home, send it to pages controller with home action.
   get '/home', to: 'pages#home'
+  
+  # # If there's a GET request for recipes, send it to recipes controller with index action.
+  # # This page lists all recipes.
+  # # Recipes Controller => index action
+  # get '/recipes', to: 'recipes#index'
+  
+  # # If there's a GET request for recipes/new, send it to recipes controller with new action.
+  # # This page contains a form that allows users/chefs to create new recipes.
+  # # Recipes Controller => new action
+  # get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
+  
+  # # This is a POST request that handles the submission of the form.
+  # # This will be handled by the create action in the recipes controller.
+  # # Recipes Controller => create action
+  # post '/recipes', to: 'recipes#create'
+  
+  # # If there's a GET request for recipes/edit, send it to recipes controller with edit action.
+  # # This page contains a form that allows users/chefs to edit their associated recipes.
+  # # Recipes Controller => edit action
+  # get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
+  
+  # # When you submit the edit, it will be a patch request to update the recipe entry.
+  # # Recipes Controller => update action
+  # patch '/recipes/:id', to: 'recipes#update'
+  
+  # # Get request to show an individual recipe (a recipe's own page)
+  # # Recipes Controller => show action
+  # get '/recipes/:id', to: 'recipes#show', as: 'recipe'
+  
+  # # Delete request to be able to delete a selected recipe entry (assuming 
+  # # the individual doing the deletion is the author of that recipe).
+  # # Recipes Controller => destroy action
+  # delete '/recipes/:id', to: 'recipes#destroy'
+  
+  
+  # Do the above commented code, but in one line.
+  resources :recipes
+  
+  
+  
+  
+  
+  ################# AUTO GENERATED CODE BELOW   #################
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
