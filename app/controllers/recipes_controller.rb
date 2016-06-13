f8c87a89d49ef9a9f84225d07937c26731f1d9b8
Abhishek <awittlegupta@gmail.com>
2016-06-13 00:09:43 +0000

    Implemented recipe submission via form and new action. Setup alert messages and form error checking.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,13 +4,37 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
   
-  def create
-    
+  def new
+    @recipe = Recipe.new
   end
   
-  def new
+  def create
+    # Takes the new object created and uses the
+    # recipe_params function defined below in
+    # private section to mass assign/create new
+    # recipes based on recipe_params which
+    # whitelists all submitted data from the form.
+    @recipe = Recipe.new(recipe_params)
     
-  end
+    # A recipe must be associated with a chef. For
+    # now, we will assign every recipe to user 2 
+    # (Abhishek Gupta).
+    @recipe.chef = Chef.find(1)
+    
+    # If the recipe was successfully saved to the
+    # database, ...
+    if @recipe.save
+      # then let the user know that via a flash notification
+      flash[:success] = "Success! Your recipe went live!"
+      
+      # and then redirect the user to the recipes page.
+      redirect_to recipes_path
+    else
+      # However, if there was a failure to save to
+      # the database, redirect user to the new page.
+      render :new
+    end
+  end  
   
   def edit
     
@@ -27,4 +51,12 @@ class RecipesController < ApplicationController
   def destroy
     
   end
+  
+  private
+    
+    def recipe_params
+      # Whitelist all relevant parameters to pass through from form 
+      # submission.
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
 end
