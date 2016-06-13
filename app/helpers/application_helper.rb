083e77b1bf8f025704cdb3b6a81e69d6be86ca6f
Abhishek <awittlegupta@gmail.com>
2016-06-13 00:21:54 +0000

    Set up image precompilation for Heroku.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,16 @@
 module ApplicationHelper
+  def asset_exist?(path)
+    
+    # If line 32 in production.rb is true (config.assets.compile = true)
+    if Rails.configuration.assets.compile
+      # Attaches alphanumeric string to it and then finds it in assets folder.
+      Rails.application.precompiled_assets.include? path
+    else
+      # Assets should be in assets/images and not alphanumeric string.
+      Rails.application.assets_manifest.assets[path].present?
+    end
+  end  
+  
   def gravatar_for(chef, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(chef.email.downcase)
     size = options[:size]
